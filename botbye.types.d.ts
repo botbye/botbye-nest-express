443c369a3f839c1347c154b5b7f81216f32b52cb
botbye <accounts@botbye.com>
2025-08-06 11:43:48 +0300

    init

---
+++ botbye.types.d.ts
@@ -0,0 +1,4 @@
+import { TBotByeServiceOptions } from "botbye-nest-core";
+import { type Request } from "express";
+type TBotByeModuleOptions = TBotByeServiceOptions<Request>;
+export { TBotByeModuleOptions, };
